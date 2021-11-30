2c273bcb611acddac90c4a3e963e9d13c65810f6
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-30 18:24:29 +0300

    feat: separate into components

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Banner from '../banner/Banner';
+
+function App() {
+  return (
+    <div className="app">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
