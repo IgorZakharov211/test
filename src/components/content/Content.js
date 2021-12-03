9882b93f920404b0327d5e15b8aa2eba94a2f0f4
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-12-03 13:29:56 +0300

    feat: add routes

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -2,12 +2,17 @@ import React from "react";
 import './Content.scss';
 import Steps from "../steps/Steps";
 import How from "../how/How";
+import { Routes, Route } from 'react-router-dom';
+import Product from "../product/Product";
 
 function Content(){
   return(
     <main className="content">
-       <Steps className="content"/>
-       <How className="content" />
+      <Routes>
+        <Route exact path="/" element={<Steps className="content"/> }> </Route>
+        <Route exact path="/product" element={<Product className="content" /> }> </Route>
+      </Routes>
+      <How className="content" />
     </main>
   )
 }
